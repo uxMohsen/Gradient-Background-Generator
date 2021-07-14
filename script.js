b874770ef0bb3c5145843643ea282a352be85a00
Mohsen <uxmohsen@gmail.com>
2021-07-14 06:14:51 +0200

    updated files

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const bg = document.querySelector(".body");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 
+
 const setBG = () => {
 	bg.style.background = 
 	"linear-gradient(to right,"
@@ -13,6 +14,7 @@ const setBG = () => {
 
 	h3.textContent = bg.style.background
 }
+
 
 color1.addEventListener("input", setBG);
 color2.addEventListener("input", setBG);
